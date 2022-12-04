e5211a2a5fbb94b04ad0dfc2c2945cd537033cfa
fovegage <fovegage@gmail.com>
2022-12-04 14:51:02 +0800

    feat: update dial

--- scripts/goproxy_install_auto.sh
+++ scripts/goproxy_install_auto.sh
@@ -64,4 +64,4 @@ cp -f /root/dial-proxy /usr/bin/
 chmod +x /usr/bin/dial-proxy
 
 # start dial proxy
-nohup dial-proxy --net ppp --time 60 >nohup.out & echo "\n"
+nohup dial-proxy >nohup.out & echo "\n"
